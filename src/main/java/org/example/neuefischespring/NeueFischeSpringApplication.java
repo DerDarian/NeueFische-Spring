fc7d25d750311fa79576a5c0df120151302d1279
azurias <karriere-prusac@mailbox.org>
2025-07-18 09:40:40 +0200

    Initial commit

---
+++ src/main/java/org/example/neuefischespring/NeueFischeSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.neuefischespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeueFischeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeueFischeSpringApplication.class, args);
+    }
+
+}
